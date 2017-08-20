6df89aa94ed9fe29a3cfe3ae4710cf2566ca7f40
jim ji <1035363309@qq.com>
2017-08-20 21:43:01 +0800

    加饼状图

--- app/src/main/java/com/example/kk/dididache/ui/MainActivity.kt
+++ app/src/main/java/com/example/kk/dididache/ui/MainActivity.kt
@@ -98,6 +98,7 @@ class MainActivity : BaseActivity() {
         }
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
